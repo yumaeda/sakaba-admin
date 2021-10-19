dbd7f4594cb38caf17b498bf766e02d5a7e448a0
Yukitaka Maeda <yumaeda@gmail.com>
2021-10-19 18:23:21 +0900

    Fix broken layout of the admin home

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -10,7 +10,7 @@ const HomePage: React.FC = () => {
             <header className="header">
                 <h1 className="header-title">{`管理者ページ`}</h1>
             </header>
-            <div>
+            <div className="contents">
                 <h2>管理者ホーム</h2>
                 <div>
                     <Link to="/menu-edit">メニューの編集</Link>
