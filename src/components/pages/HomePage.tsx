13be518d55611f3693152d695b2e5c3b436e9ce0
Yukitaka Maeda <yumaeda@gmail.com>
2021-11-15 18:45:16 +0900

    Add eslint

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -4,20 +4,18 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
 
-const HomePage: React.FC = () => {
-    return (
-        <>
-            <header className="header">
-                <h1 className="header-title">{`管理者ページ`}</h1>
-            </header>
-            <div className="contents">
-                <h2>管理者ホーム</h2>
-                <div>
-                    <Link to="/menu-edit">メニューの編集</Link>
-                </div>
-            </div> 
-        </>
-    )
-}
+const HomePage: React.FC = () => (
+  <>
+    <header className="header">
+      <h1 className="header-title">管理者ページ</h1>
+    </header>
+    <div className="contents">
+      <h2>管理者ホーム</h2>
+      <div>
+        <Link to="/menu-edit">メニューの編集</Link>
+      </div>
+    </div>
+  </>
+)
 
 export default HomePage
